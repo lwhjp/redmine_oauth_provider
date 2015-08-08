711f4d8a534c9d59609259f16a373d145b832586
Ryo SUETSUGU <suetsugu.r@gmail.com>
2015-08-08 20:11:49 +0900

    Bumped: 0.0.4

--- init.rb
+++ init.rb
@@ -20,7 +20,7 @@ Redmine::Plugin.register :redmine_oauth_provider do
   name 'Redmine Oauth Provider plugin'
   author 'Jana Dvořáková'
   description 'Oauth Provider plugin for Redmine'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/Virtualmaster/redmine-oauth-provider'
   author_url 'http://www.jana4u.net/'
 end
