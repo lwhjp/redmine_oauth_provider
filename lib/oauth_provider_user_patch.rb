f4a611dff667d38d13b465935c50d817956c8f20
Irmela <irmela@berlin-coding.de>
2015-10-12 17:20:07 +0200

    making migrations work in redmine3/rails4 by changing order of has_many arguments

--- lib/oauth_provider_user_patch.rb
+++ lib/oauth_provider_user_patch.rb
@@ -5,7 +5,7 @@ module OauthProviderUserPatch
       unloadable
 
       has_many :client_applications
-      has_many :tokens, {:class_name => "OauthToken"},  ->{includes(:client_application).order("authorized_at desc")}
+      has_many :tokens, -> {includes(:client_application).order("authorized_at desc")}, {:class_name => "OauthToken"}
     end
   end
 end
